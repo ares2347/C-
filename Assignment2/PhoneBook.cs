f94781c6c88906989560ef971268922552215232
hoagtu <trantu2000hp@gmail.com>
2022-12-27 13:59:20 +0700

    Initial commit

---
+++ Assignment2/PhoneBook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class PhoneBook : Phone
+    {
+        public List<PhoneModel> PhoneList { get; set; }
+        public override void InsertPhone(string name, string phone)
+        {
+            if (PhoneList.Exists(x => x.Name == name))
+            {
+                var exist = PhoneList.FirstOrDefault(x => x.Name == name);
+                if(!exist.Phone.Contains(phone))
+                {
+                    exist.Phone.Add(phone); 
+                }
+            }
+            else
+            {
+                PhoneList.Add(new PhoneModel
+                {
+                    Name = name,
+                    Phone = new List<string>() { phone}
+                }) ;
+            };
+        }
+
+        public override void RemovePhone(string name)
+        {
+            PhoneList.Remove(PhoneList.FirstOrDefault(x => x.Name == name));
+        }
+
+        public override List<string> SearchPhone(string name)
+        {
+            return PhoneList.FirstOrDefault(x => x.Name == name).Phone;
+        }
+
+        private static int PhoneComparer(PhoneModel x, PhoneModel y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    public override void Sort()
+        {
+            PhoneList.Sort(PhoneComparer);
+        }
+
+        public override void UpdatePhone(string name, string newPhone)
+        {
+            var exist = PhoneList.FirstOrDefault(x => x.Name==name);
+            exist.Phone = new List<string> { newPhone };
+        }
+    }
+}
---
+++ Assignment2/PhoneModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class PhoneModel
+    {
+        public string Name { get; set; }
+        public List<string> Phone { get; set; }
+    }
+}
