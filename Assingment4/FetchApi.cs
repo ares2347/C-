e88e9b62423b8d0fb0cf76f8d5112d492c953591
hoagtu <trantu2000hp@gmail.com>
2023-01-03 19:44:28 +0700

    Assignment4

---
+++ Assingment4/FetchApi.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Assingment4
+{
+    internal class FetchApi
+    {
+        private readonly string _url = "https://api.openweathermap.org/data/2.5/weather?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+        public async Task<WeatherData> Fetch()
+        {
+            var client = new HttpClient();
+            try
+            {
+                var response = await client.GetAsync(_url);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var res = await response.Content.ReadAsStringAsync();
+                    var weather = JsonConvert.DeserializeObject<WeatherData>(res);
+                    return weather;
+                }
+                return null;
+            }
+            catch(Exception ex) { throw ex; }
+        }
+    }
+}
---
+++ Assingment4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assingment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fetch();
+        }
+
+        static async void Fetch()
+        {
+            var res = await new FetchApi().Fetch();
+            Console.WriteLine(res.Name.ToString());
+        }
+    }
+}
